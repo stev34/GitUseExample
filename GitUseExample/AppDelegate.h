9847c961d56386e1ff48b9786c0c1a0d13d7fc6f
jb <jeanbaptistestevenard@macbook-pro-de-jean-baptiste-stevenard.local>
2012-12-21 11:28:15 +0100

    Initial Commit

---
+++ GitUseExample/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  GitUseExample
+//
+//  Created by jb on 21/12/12.
+//  Copyright (c) 2012 jb. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ GitUseExample/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  GitUseExample
+//
+//  Created by jb on 21/12/12.
+//  Copyright (c) 2012 jb. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
